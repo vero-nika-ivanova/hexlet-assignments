56f19e467fdd765f8e08aad634fe7d7029285dc6
Вероника Иванова <veronika@MacBook-Air-Veronika.local>
2024-05-20 23:31:29 +0200

    Initial commit

---
+++ java-spring-ru/first-start/build.gradle.kts
@@ -0,0 +1,39 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    // BEGIN
+    
+    // END
+    application
+    id("com.github.ben-manes.versions") version "0.48.0"
+}
+
+group = "exercise"
+
+version = "1.0-SNAPSHOT"
+
+application { mainClass.set("exercise.Application") }
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // BEGIN
+    
+    // END
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testImplementation("org.hamcrest:hamcrest:2.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        showStandardStreams = true
+    }
+}
---
+++ java-spring-ru/first-start/src/main/java/exercise/Application.java
@@ -0,0 +1,10 @@
+package exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// BEGIN
+
+// END
