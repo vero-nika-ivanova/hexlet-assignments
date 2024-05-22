2941d912d5f9d104646328a2b18fbae202e13e42
@hexlet/cli <support@hexlet.io>
2024-05-23 01:10:27 +0200

    submit java-spring-ru/annotations

--- java-spring-ru/annotations/src/main/java/exercise/Application.java
+++ java-spring-ru/annotations/src/main/java/exercise/Application.java
@@ -1,8 +1,7 @@
 package exercise;
 
-import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // BEGIN
